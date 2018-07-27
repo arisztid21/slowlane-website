b975ea646ba8a83d0d57b81436d4123862975be9
arisztid munoz <arisztidmunoz@arisztids-MacBook-Pro.local>
2018-07-27 15:00:23 -0700

    work on allbios

---
+++ src/components/bios/all_bios.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class Familia extends Component{
+    render(){
+        return(
+            <div>
+                <div classname = "vato">
+                    <h2>Vato</h2>
+                    <img/>
+                    <p>
+                        As a baby i grew up around cars. My dad cruised and so did his dad and mom before him.
+                        My dad and tios owned a paint and body so there was always something with cars happening in my family.
+                        I've always been dedicated to the lowriding lifestyle, fixing and restoring cars been an interest, just watching the process
+                        of a car being restored to its original glory if not better from start to finish. Lowriding has always been an art for me, i always 
+                        tried to out do myself from build to build. I've introduced lowriding to my kids just to another possibble door for them,
+                         if they find interest I will do the best i can to help build that want, to continue the lifestyle. Lowriding kept me interested because 
+                         i've realized that places where people didn't get along, it had them together to all share the common interest, because 
+                         of that i've met and know so many people. It is a life i only wish to continue to pursue.
+                    </p>
+                </div>
+                <div className = "robert">
+                    <h2>Robert</h2>
+                    <img/>
+                    <p>
+                        i start
+                    </p>
+                </div>
+                <div className = "jesse"></div>
+                <div className = "jd"></div>
+                <div className = "joker"></div>
+                <div className = ""></div>
+                <div className = ""></div>
+                <div className = ""></div>
+                <div className = ""></div>
+                <div className = ""></div>
+            </div>
+        )
+    }
+}
+
+export default Familia;
